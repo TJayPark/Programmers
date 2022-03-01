c3d682744b8a1a7991b809e9a6b3fc6d0bb31f2e
TjayPark <63952252+TJayPark@users.noreply.github.com>
2022-03-01 08:42:32 +0000

    file1 = 숫자게임

--- greedy/file1.py
+++ greedy/file1.py
@@ -1,1 +1,25 @@
-print(1)
+t# N, M을 공백을 기준으로 입력 받기
+n, m = map(int, input().split())
+
+arr = []
+minVal = 0
+for i in range(n):    
+	arr.append(list(map(int, input().split())))
+
+maxNum = None
+for i in arr:
+  num = None
+  for j in i:
+    if(num==None):
+      num = j
+    else:
+      if(j<num):
+        num = j
+  if(maxNum == None):
+    maxNum = num
+  else:
+    if(num>maxNum):
+      maxNum = num
+            
+print(maxNum)  
+      
